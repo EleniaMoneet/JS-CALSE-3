18a9da602613b0df01fadaf4c68febfe7b0ab67c
Ayelen Gabrielli <moneet.moneet@gmail.com>
2021-08-07 23:22:11 -0300

    COMMIT UNO

---
+++ app.js
@@ -0,0 +1,22 @@
+
+
+
+// Sobre Pounch de comida para gatos
+
+let pounch = 100
+
+let cantidad = Number(prompt("Cantidad Solicitada por Unidad"))
+
+function datos(cant) {  
+
+console.log((cant/ 1 ) * pounch);
+}
+ datos (cantidad)
+
+
+ 
+
+
+ 
+ 
+ 
